32907094de492ff47b4669eda18062e0e78b5a53
PedroFreitas <pedrosteamgtav@gmail.com>
2025-05-11 21:51:54 -0300

    make errors to test

--- __tests__/math.test.ts
+++ __tests__/math.test.ts
@@ -1,7 +1,19 @@
-import { add, sub, div, mul } from "../src/math";
+import { add } from "../src/math";
 
 describe('Math test', () => {
-    test('add 1 plus 2 equals to 3', () => {
+    test('add 1 to 2 equals to 3', () => {
         expect(add(1, 2)).toEqual(3);
+    });
+
+    test('sub 2 by 1 equals to 1', () => {
+        expect(sub(2, 1)).toEqual(1);
+    });
+
+    test('Divide 2 by 1 equals to 1', () => {
+        expect(div(2, 1)).toEqual(1);
+    });
+
+    test('Multiply 2 by 2 equals to 4', () => {
+        expect(mul(2, 2)).toEqual(1);
     });
 })
--- src/math.ts
+++ src/math.ts
@@ -2,14 +2,14 @@ export function add(x: number, y: number): number {
     return x + y;
 }
 
-export function sub(x: number, y: number): number {
-    return x - y;
-}
+// export function sub(x: number, y: number): number {
+//     return x - y;
+// }
 
-export function div(x: number, y: number): number {
-    return x / y;
-}
+// export function div(x: number, y: number): number {
+//     return x / y;
+// }
 
-export function mul(x: number, y: number): number {
-    return x * y;
-}
+// export function mul(x: number, y: number): number {
+//     return x * y;
+// }
