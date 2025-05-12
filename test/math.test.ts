93a4cc95f68f74285784a4987a1acdba06888eb2
PedroFreitas <pedrosteamgtav@gmail.com>
2025-05-11 21:13:17 -0300

    fixed math.test.ts added gitignore and ci.yml

--- test/math.test.ts
+++ test/math.test.ts
@@ -1,5 +1,5 @@
-import { add, sub, div, mul } from "../math";
+import { add, sub, div, mul } from "../src/math";
 
 test('add 1 plus 2 equals to 3', () => {
-    expected(add(1, 2).toBe(3));
+    expect(add(1, 2)).toEqual(3);
 });
